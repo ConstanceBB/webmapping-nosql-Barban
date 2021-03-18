1b3cf7279114fa7f5eb644c1842f9c4793334a1a
formation <formation@ubuntu.ubuntu-domain>
2021-03-18 09:23:13 +0100

    premierCommit

---
+++ index.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var app = express();
+var multer = require('multer');
+var upload = multer();
+var bodyParser = require('body-parser');
+var mydb;
+var MongoClient = require('mongodb').MongoClient;
+
+//To parse URL encoded data
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//To parse json data
+app.use(bodyParser.json())
+
+app.use('/static', express.static('public')); //autorise à utiliser les static file
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/geo-search', function(req, res){
+   res.render('form');
+});
+
+
+MongoClient.connect("mongodb://localhost:27017/geo", function(err, db) {
+ console.log("Connected correctly to server");
+
+ mydb = db;
+ app.listen(3000);
+ console.log("Server listening...");
+});
+
+
+app.get('/geo-search-results', function(req, res){
+   //console.log(req.query); // retourne les infos rentrées par l'utilisateur
+
+   var latitude = parseFloat(req.query.latitude);
+   var longitude = parseFloat(req.query.longitude);
+   var radius = parseFloat(req.query.Radius);
+
+   var filter = {};
+    filter.geometry = { "$geoWithin": { "$center": [ [ longitude ,latitude] , radius ] } };
+
+    console.log("filter", JSON.stringify(filter));
+
+    mydb.collection('equip').find(filter).toArray(function(err, docs) {
+       console.log("Found "+docs.length+" records");
+       //console.dir(docs);
+       res.render('geo-search-results', {
+         results: docs
+       });
+     });
+
+});
+
+
+app.get('/geo-search-results-json', function(req, res){
+   //console.log(req.query); // retourne les infos rentrées par l'utilisateur
+
+   var latitude = parseFloat(req.query.latitude);
+   var longitude = parseFloat(req.query.longitude);
+   var radius = parseFloat(req.query.Radius);
+
+   var filter = {};
+    filter.geometry = { "$geoWithin": { "$center": [ [ longitude ,latitude] , radius ] } };
+
+    console.log("filter", JSON.stringify(filter));
+
+    mydb.collection('equip').find(filter).toArray(function(err, docs) {
+       console.log("Found "+docs.length+" records");
+
+       res.set('Content-Type', 'application/json; charset utf-8');
+       res.end(JSON.stringify(
+         {
+           "type": "FeatureCollection",
+           "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+           "features": docs
+         }));
+     });
+});
---
+++ public/map.js
@@ -0,0 +1,82 @@
+/**
+ * Elements that make up the popup.
+ */
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+
+/**
+ * Create an overlay to anchor the popup to the map.
+ */
+var overlay = new ol.Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250,
+  },
+});
+
+/**
+ * Create a vector layer with the result of the requete
+ */
+var coucheEquipement = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: 'geo-search-results-json' + window.location.search,
+    format: new ol.format.GeoJSON()
+  }),
+  style: new ol.style.Style({
+    image: new ol.style.Circle({
+      radius:10,
+      stroke: new ol.style.Stroke({
+        color: 'red',
+        width: 4
+      }),
+      fill: new ol.style.Fill({
+        color: 'olive'
+      })
+    })
+    })
+
+  });
+
+/**
+* Create a map with layers
+*/
+var map = new ol.Map({
+ target: 'map',
+ overlays: [overlay],
+ layers: [
+   new ol.layer.Tile({
+     source: new ol.source.OSM(),
+     opacity: 1
+   }),
+   coucheEquipement],
+   view: new ol.View({
+     center: ol.proj.fromLonLat([2.35, 48.841]),
+     zoom: 12,
+     maxZoom: 20,
+   }),
+
+
+});
+
+
+/**
+ * Add a click handler to the map to render the popup.
+ */
+map.on('singleclick', function (evt) {
+  var coordinate = evt.coordinate;
+  var hdms = ol.coordinate.toStringHDMS(ol.proj.toLonLat(coordinate));
+  content.innerHTML = '<p>You clicked here:</p><code>' + hdms + '</code>';
+  overlay.setPosition(coordinate);
+});
+
+/**
+ * Add a click handler to hide the popup.
+ * @return {boolean} Don't follow the href.
+*/
+closer.onclick = function () {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
