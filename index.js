df21fcdbe3a7c691194e3dc34a04ea5fd9c6d9a5
formation <formation@ubuntu.ubuntu-domain>
2021-03-19 14:57:18 +0100

    AjoutElasticsearch

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var multer = require('multer');
 var upload = multer();
 var bodyParser = require('body-parser');
+const elasticsearch = require("elasticsearch")
 var mydb;
 var MongoClient = require('mongodb').MongoClient;
 
@@ -24,11 +25,18 @@ app.use(bodyParser.json());
 // for parsing application/xwww-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/geo-search', function(req, res){
-   res.render('form');
+
+//Version avec leasticsearch
+app.listen(process.env.PORT || 3000, () => {
+    console.log("connected")
+});
+const esClient = elasticsearch.Client({
+    host: "http://127.0.0.1:9200",
 });
 
 
+
+/** version avec une BDD mongo
 MongoClient.connect("mongodb://localhost:27017/geo", function(err, db) {
  console.log("Connected correctly to server");
 
@@ -36,19 +44,61 @@ MongoClient.connect("mongodb://localhost:27017/geo", function(err, db) {
  app.listen(3000);
  console.log("Server listening...");
 });
+*/
 
+app.get('/geo-search', function(req, res){
+   res.render('form');
+});
 
+//Version avec BDD elastic search
+app.get("/products", (req, res) => {
+  var latitude = parseFloat(req.query.latitude);
+  var longitude = parseFloat(req.query.longitude);
+  var radius = parseFloat(req.query.Radius);
+  var titre = req.query.titre;
+
+    esClient.search({
+        index: "equi", //nom de la base
+        body: {
+            query: {
+              "bool": {
+                "must": {
+                  "match_all": {}
+                },
+                "filter": {
+                  "geo_distance": {
+                  "distance": radius+"km",
+                  "coordinates": [
+                    longitude,
+                    latitude
+                  ]
+                }
+                }
+
+              }
+            }
+        }
+    })
+    .then(response => {
+        res.render('geo-search-results',{
+          results : response.hits.hits.map(function(hit){
+            return { "properties" : hit._source ,"geometry":{ "coordinates" : hit._source.coordinates } }
+          })
+        })
+    })
+    .catch(err => {
+        return res.status(500).json({"message": "Error"})
+    })
+});
+
+//VErsion BDD avec mongo
 app.get('/geo-search-results', function(req, res){
-   //console.log(req.query); // retourne les infos rentrées par l'utilisateur
 
    var latitude = parseFloat(req.query.latitude);
    var longitude = parseFloat(req.query.longitude);
    var radius = parseFloat(req.query.Radius);
    var titre = req.query.titre;
 
-   //var expressionregu = '/'.concat('',titre);
-   //titre = expressionregu.concat('', '/');
-   console.log(titre);
 
    var filter = {"properties.ins_nom": {$regex : ".*"+titre+".*", $options : "i"}};
    filter.geometry = { "$geoWithin": { "$center": [ [ longitude ,latitude] , radius ] } };
@@ -64,14 +114,15 @@ app.get('/geo-search-results', function(req, res){
 });
 
 
+//VERSION POUR SORTIR UN JSON --> sert au fichier map.js pour afficher la carte
 app.get('/geo-search-results-json', function(req, res){
-   console.log("coucou ? ");
+
    var latitude = parseFloat(req.query.latitude);
    var longitude = parseFloat(req.query.longitude);
    var radius = parseFloat(req.query.Radius);
+   var titre = req.query.titre;
 
-
-   var filter = {};
+   var filter = {"properties.ins_nom": {$regex : ".*"+titre+".*", $options : "i"}};
     filter.geometry = { "$geoWithin": { "$center": [ [ longitude ,latitude] , radius ] } };
 
     console.log("filter", JSON.stringify(filter));
